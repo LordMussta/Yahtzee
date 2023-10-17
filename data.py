8f57ec6a972f58fca59439f4fd984c71141705ca
Mussta <mussta1984@gmail.com>
2023-10-18 06:15:43 +1000

    Good progress on highscores chart

---
+++ data.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+connection = sqlite3.connect("data.db")
+
+
+def create_table():
+    with connection:
+        connection.execute(
+            "CREATE TABLE IF NOT EXISTS highScores (date TEXT, user TEXT, score INTEGER);")
+
+
+def insert_data(date, user, score):
+    with connection:
+        connection.execute(
+            "INSERT INTO highScores VALUES(?, ?, ?);", (date, user, score))
+
+
+def select_all_data():
+    return connection.execute("SELECT * FROM highScores ORDER BY score DESC LIMIT 10;")
+
+
+def close_connection():
+    connection.close()
